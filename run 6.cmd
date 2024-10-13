2eb7cc5683dfe05d7115823bc1c2dd372604e40f
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2024-10-13 09:52:10 -0300

    Update run 6.cmd

--- run 6.cmd
+++ run 6.cmd
@@ -1,5 +1,5 @@
 @echo off
-echo ******** SCRIPT - Instalacao da superimageVirtual v8.1 - AGENCIAS ********
+echo ******** SCRIPT - Instalacao da superimageVirtual v8.1 ********
 :inicio
 for /F "tokens=1-2" %%a in (IP.txt) do start /min principal.cmd %%a & sleep 50
 rem goto inicio
