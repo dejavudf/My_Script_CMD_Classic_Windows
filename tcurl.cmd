9ad68f206fe5dc8c8579dac7908161feaf8fc615
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2025-11-04 09:02:39 -0300

    Update tcurl.cmd

--- tcurl.cmd
+++ tcurl.cmd
@@ -1,13 +1,6 @@
-@echo off
-
-REM variables
-set VAR_DATE_START=%date%
-set VAR_TIME_START=%time%
-set VAR_COUNT=30
-
-echo Site: %1 >> url_curl.log
-echo Data/Hora Inicio: %VAR_DATE_START% - %VAR_TIME_START% >> url_curl.log
-curl -v -I %1 -m 5 | findstr HTTP >> url_curl.log
-echo Data/Hora Inicio: %date% - %time% >> url_curl.log
-echo ############################################################################ >> url_curl.log
+@echo on
+echo "%date% - %time%" >> %2_curl.log
+echo "Site: %1" >> %2_curl.log
+curl -v -I %1 -m 5 | findstr HTTP >> %2_curl.log
+echo ################################################################################ >> %2_curl.log
 exit
